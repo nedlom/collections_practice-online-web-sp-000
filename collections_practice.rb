f42ecb4f42962991157662061fc83e4cae4fd563
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-14 18:37:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
+def sort_array_asc(array)
+  array.sort
+end   
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1 
+      array[2]
+    elsif index == 2
+      array[1]
+    else
+      element
+    end
+  end
+end
+
+def reverse_array(array)
+  array.collect{|int| int}.reverse
+end
+
+def kesha_maker(array)
+  array.collect do |string|
+    string[2] = "$"
+    string
+  end
+end
+
+def find_a(array)
+  array.select do |string|
+    string.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject{|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index != 1
+      element + "s"
+    else
+      element
+    end
+  end
+end
