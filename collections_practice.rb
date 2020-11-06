b746453f6814421e3549df3214f6d2536febf816
imperative-scientist-4600 <nelom3@gmail.com>
2020-11-06 20:43:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,10 +13,7 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  two = array.slice(1)
-  three = array.slice(2)
-  array[1] = three
-  array[2] = two
+  array[1], array[2] = array[2], array[1]
   array
 end
 
